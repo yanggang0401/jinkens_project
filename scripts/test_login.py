70256ae3837dcfbd67b386e5a5b86418790ae792
yanggang0401 <1458525557@qq.com>
2019-08-30 14:23:51 +0800

    第一次初始化项目

---
+++ scripts/test_login.py
@@ -0,0 +1,9 @@
+class TestLogin:
+    def test_login1(self):
+        print("test_login1")
+
+    def test_login2(self):
+        print("test_login2")
+
+    def test_login3(self):
+        print("test_login3")
