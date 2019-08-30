8d69f9aa80e3df4ad12df7469cb76723121f417c
yanggang0401 <1458525557@qq.com>
2019-08-30 16:18:06 +0800

    增加了一个不稳定的因素，更改了第二个用例

--- scripts/test_login.py
+++ scripts/test_login.py
@@ -1,9 +1,13 @@
+import random
+
+
 class TestLogin:
     def test_login1(self):
         print("test_login1")
 
     def test_login2(self):
-        print("test_login2")
+        num = random.randint(0, 2)
+        assert not num
 
     def test_login3(self):
         print("test_login3")
